cf4bea4aac5207b77509b9000af677ed99aad5fe
Fred <fred>
2015-10-22 19:07:56 +0200

    First commit

---
+++ NUnitTests/MessageBusTests.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using WinFwk;
+
+namespace NUnitTests
+{
+    [TestFixture]
+    public class MessageBusTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [TearDown] 
+        public void teardown()
+        {
+
+        }
+
+        [Test]
+        public void GetMessageTypesTest()
+        {
+            var l = MessageBus.GetMessageTypes(new MockSimpleSubscriber());
+            Assert.That(l, Is.Not.Null);
+
+        }
+    }
+
+    public class StatusMsg
+    {
+        public string Text { get; set; }
+    }
+
+    public class ResetMsg
+    {
+        public string Reason { get; set; }
+    }
+
+    public class MockSimpleSubscriber : IMessageListener<StatusMsg>
+    {
+        public List<StatusMsg> StatusMsgs { get; } = new List<StatusMsg>();
+        void IMessageListener<StatusMsg>.HandleMessage(StatusMsg message)
+        {
+            StatusMsgs.Add(message);
+        }
+    }
+
+    public class MockdualSubscriber : MockSimpleSubscriber , IMessageListener<ResetMsg>
+    {
+        public List<ResetMsg> ResetMsgs { get; } = new List<ResetMsg>();
+        void IMessageListener<ResetMsg>.HandleMessage(ResetMsg message)
+        {
+            ResetMsgs.Add(message);
+        }
+    }
+}
---
+++ WinFwk/MessageBus/MessageBus.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WinFwk
+{
+    public interface IMessageListener<T>
+    {
+        void HandleMessage(T message);
+    }
+
+    public class MessageBus
+    {
+        Dictionary<Type, List<object>> dicoSubscribers = new Dictionary<Type, List<object>>();
+        static internal IEnumerable<Type> GetMessageTypes(object subscriber)
+        {
+            List<Type> types = new List<Type>();
+
+            if (subscriber == null)
+            {
+                return types;
+            }
+
+            Type type = subscriber.GetType();
+            var interfaces = type.GetInterfaces();
+            foreach (var interfType in interfaces)
+            {
+                if (interfType.IsAssignableFrom(typeof(IMessageListener<>)))
+                {
+                    var genArgs = interfType.GetGenericArguments();
+                    foreach (var genArg in genArgs)
+                    {
+                        types.Add(genArg);
+                    }
+                }
+            }
+            return types;
+        }
+
+        public List<object> GetSubscribers(Type msgType)
+        {
+            List<object> subscribers;
+            if (!dicoSubscribers.TryGetValue(msgType, out subscribers))
+            {
+                subscribers = new List<object>();
+                dicoSubscribers.Add(msgType, subscribers);
+            }
+
+            return subscribers;
+        }
+
+        public void Subscribe(object subscriber)
+        {
+            if(subscriber == null)
+            {
+                return;
+            }
+
+            var messageTypes = GetMessageTypes(subscriber);
+            foreach(var msgType in messageTypes)
+            {
+                var subscribers = GetSubscribers(msgType);
+                if( ! subscribers.Contains(subscriber)  )
+                {
+                    subscribers.Add(subscriber);
+                }
+            }
+        }
+
+        public void Unsubscribe(object subscriber)
+        {
+            if (subscriber == null)
+            {
+                return;
+            }
+
+            var messageTypes = GetMessageTypes(subscriber);
+            foreach (var msgType in messageTypes)
+            {
+                var subscribers = GetSubscribers(msgType);
+                if (!subscribers.Contains(subscriber))
+                {
+                    subscribers.Remove(subscriber);
+                }
+            }
+        }
+    }
+}
