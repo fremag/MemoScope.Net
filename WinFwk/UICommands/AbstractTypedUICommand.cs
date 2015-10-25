cea78c8fa5867b88b72981eee97d630d750fae5d
Fred <fred>
2015-10-25 22:58:39 +0100

    [Workplace] Enhancements: handle added, removed modules

---
+++ WinFwk/UICommands/AbstractTypedUICommand.cs
@@ -0,0 +1,35 @@
+﻿using System.Drawing;
+using WinFwk.UIModules;
+
+namespace WinFwk.UICommands
+{
+    public abstract class AbstractTypedUICommand<T> : AbstractUICommand
+    {
+        private UIDataProvider<T> dataProvider;
+
+        protected AbstractTypedUICommand(string name, string toolTip, string @group, Icon icon) : base(name, toolTip, @group, icon)
+        {
+            Enabled = false;
+        }
+
+        // Abstract
+        protected abstract void HandleData(T data);
+
+        public override void SetSelectedModule(UIModule module)
+        {
+            dataProvider = module as UIDataProvider<T>;
+            Enabled = (dataProvider != null);
+        }
+
+        public override void Run()
+        {
+            if (dataProvider == null)
+            {
+                return;
+            }
+
+            var data = dataProvider.Data;
+            HandleData(data);
+        }
+    }
+}
