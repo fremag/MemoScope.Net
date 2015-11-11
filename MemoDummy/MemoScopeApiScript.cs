46df2e052e34d0d418c9791f46476bd84398596e
fremag <frederic.magri@gmail.com>
2015-11-11 19:20:08 +0100

    MemoDummy: add a script to test MemoScopeApi Closes #16

---
+++ MemoDummy/MemoScopeApiScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using MemoScopeApi;
+
+namespace MemoDummy
+{
+    public class MemoScopeApiScript : AbstractMemoScript
+    {
+        public override string Name => "MemoScope Api Test";
+        public override string Description => "Ask MemoScope to dump MemoDumy";
+
+        public override void Run()
+        {
+            var client = new MemoScopeClient(Process.GetCurrentProcess().Id);
+            client.Open();
+            client.DumpMe();
+        }
+    }
+}
