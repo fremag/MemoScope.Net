3260628b5426f683673100edc9a53ecd958bca99
fremag <frederic.magri@gmail.com>
2015-12-12 19:11:33 +0100

    [Sql] Cache: first commit: add sqlite libs

---
+++ MemoScope/Core/Cache/ClrDumpCache.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.SQLite;
+
+namespace MemoScope.Core.Cache
+{
+    public class ClrDumpCache
+    {
+        ClrDump ClrDump { get; }
+        SQLiteConnection cxion;
+        public ClrDumpCache(ClrDump clrDump)
+        {
+            ClrDump = clrDump;
+        }
+    }
+}
