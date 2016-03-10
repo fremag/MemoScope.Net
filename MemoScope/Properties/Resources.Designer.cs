cb538752e82b3423ff98ab0b1a7d67e73ed97681
fremag <frederic.magri@gmail.com>
2016-03-10 10:56:41 +0100

    Icons: add some new icons for delegates targets module

--- MemoScope/Properties/Resources.Designer.cs
+++ MemoScope/Properties/Resources.Designer.cs
@@ -923,6 +923,26 @@ namespace MemoScope.Properties {
         /// <summary>
         ///   Recherche une ressource localisée de type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap table_lightning {
+            get {
+                object obj = ResourceManager.GetObject("table_lightning", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une ressource localisée de type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap table_lightning_small {
+            get {
+                object obj = ResourceManager.GetObject("table_lightning_small", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une ressource localisée de type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap text_rotate {
             get {
                 object obj = ResourceManager.GetObject("text_rotate", resourceCulture);
