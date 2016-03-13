acbc2372a0292247a89975d6582f16a48e4c5177
fremag <frederic.magri@gmail.com>
2016-03-13 17:25:33 +0100

    Explorer: add a command in File toobar to open one Closes #82

---
+++ MemoScope/Modules/Explorer/ExplorerCommand.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+using WinFwk.UICommands;
+using WinFwk.UIModules;
+using WinFwk.UITools.ToolBar;
+
+namespace MemoScope.Modules.Explorer
+{
+    public class ExplorerCommand : AbstractVoidUICommand
+    {
+        public ExplorerCommand() : base("Explorer", "Display explorer", UIToolBarSettings.File.Name, Properties.Resources.folders_explorer, Keys.Control|Keys.Shift|Keys.E)
+        {
+        }
+
+        public override void Run()
+        {
+            UIModuleFactory.CreateModule<ExplorerModule>(module => { }, module => DockModule(module, DockState.DockLeft));
+        }
+    }
+}
