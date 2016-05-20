f05d63468c9d43321162ec323b486908121167bb
fremag <frederic.magri@gmail.com>
2016-05-20 10:23:07 +0200

    Git: add missing file

---
+++ WinFwk/UICommands/AbstractDataUICommand.cs
@@ -0,0 +1,42 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using WinFwk.UIModules;
+
+namespace WinFwk.UICommands
+{
+    public abstract class AbstractDataUICommand<T> : AbstractUICommand
+    {
+        protected UIDataProvider<T> dataProvider;
+        protected UIModule selectedModule;
+
+        protected AbstractDataUICommand(string name, string toolTip, string group, Image icon, Keys shortcut=Keys.None ) : base(name, toolTip, group, icon, shortcut)
+        {
+            Enabled = false;
+        }
+
+        // Abstract
+        protected abstract void HandleData(T data);
+
+        public override void SetSelectedModule(UIModule module)
+        {
+            selectedModule = module;
+            InitDataProvider(module as UIDataProvider<T>);
+        }
+
+        public void InitDataProvider(UIDataProvider<T> uiDataProvider)
+        {
+            dataProvider = uiDataProvider;
+            Enabled = (dataProvider != null);
+        }
+        public override void Run()
+        {
+            if (dataProvider == null)
+            {
+                return;
+            }
+
+            var data = dataProvider.Data;
+            HandleData(data);
+        }
+    }
+}
