1beaa3241a37576c4d616ac1d730da7b34a7940e
fremag <frederic.magri@gmail.com>
2016-06-03 09:50:28 +0200

    Update RegexFilterControl.Designer.cs

--- MemoScope/Tools/RegexFilter/RegexFilterControl.Designer.cs
+++ MemoScope/Tools/RegexFilter/RegexFilterControl.Designer.cs
@@ -96,8 +96,8 @@
             // 
             // RegexFilterControl
             // 
-        //    this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.cbIgnoreCase);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnApply);
