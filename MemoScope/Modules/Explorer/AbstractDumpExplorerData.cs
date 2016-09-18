10a931fa5e7dc56171ecbd9bb03ba40513ebba8d
fremag <frederic.magri@gmail.com>
2016-09-18 16:08:38 +0200

    Process: after a dump, add a xml file with process info Closes #91

--- MemoScope/Modules/Explorer/AbstractDumpExplorerData.cs
+++ MemoScope/Modules/Explorer/AbstractDumpExplorerData.cs
@@ -16,7 +16,7 @@ namespace MemoScope.Modules.Explorer
         public abstract long Size { get; }
         public Image Icon { get; protected set; }
 
-        [OLVColumn(Title = "Date", Width = 100, TextAlign = HorizontalAlignment.Center, AspectToStringFormat = "{0:yyyy/MM/dd}")]
+        [OLVColumn(Title = "Date", Width = 150, TextAlign = HorizontalAlignment.Center, AspectToStringFormat = "{0:yyyy/MM/dd HH:mm:ss}")]
         public DateTime? Date => FileInfo?.LastWriteTime;
 
         [IntColumn(Title = "Cache Size (Mo)", Width = 50)]
@@ -36,6 +36,37 @@ namespace MemoScope.Modules.Explorer
         }
         [OLVColumn(Title = "Delete Cache", Width=100, TextAlign =HorizontalAlignment.Center)]
         public string DeleteCache => CacheSize != null ? "Delete" : null;
+
+        [OLVColumn(Title = "Machine", Width = 100)]
+        public virtual string MachineName => null;
+        [OLVColumn(Title = "User", Width = 50)]
+        public virtual string UserName => null;
+        [IntColumn(Title = "Virt Mem (Mo)", Width = 50)]
+        public virtual long? VirtualMemory => null;
+        [IntColumn(Title = "Peak Virt Mem (Mo)", Width = 50)]
+        public virtual long? PeakVirtualMemory => null;
+        [IntColumn(Title = "Paged Mem (Mo)", Width = 50)]
+        public virtual long? PagedMemory => null;
+        [IntColumn(Title = "Peak Paged Mem (Mo)", Width = 50)]
+        public virtual long? PeakPagedMemory => null;
+        [IntColumn(Title = "Peak Working Set (Mo)", Width = 50)]
+        public virtual long? PeakWorkingSet => null;
+        [IntColumn(Title = "Working Set (Mo)", Width = 50)]
+        public virtual long? WorkingSet => null;
+        [IntColumn(Title = "Private Mem", Width = 50)]
+        public virtual long? PrivateMemory => null;
+        [OLVColumn(Title = "Start Time", Width = 150, AspectToStringFormat = "{0:yyyy/MM/dd HH:mm:ss}")]
+        public virtual DateTime? StartTime => null;
+        [OLVColumn(Title = "Dump Time", Width = 150, AspectToStringFormat = "{0:yyyy/MM/dd HH:mm:ss}")]
+        public virtual DateTime? DumpTime => null;
+        [OLVColumn(Title = "Total Proc. Time", Width = 150)]
+        public virtual TimeSpan? TotalProcessorTime => null;
+        [OLVColumn(Title = "User Proc. Time", Width = 150)]
+        public virtual TimeSpan? UserProcessorTime => null;
+        [OLVColumn(Title = "Command Line", Width = 150)]
+        public virtual string CommandLine => null;
+        [IntColumn(Title = "Handles", Width = 50)]
+        public virtual long? HandleCount => null;
 
         public virtual string GetCachePath()
         {
--- MemoScope/Modules/Explorer/FileData.cs
+++ MemoScope/Modules/Explorer/FileData.cs
@@ -1,4 +1,6 @@
 using MemoScope.Core.Cache;
+using MemoScope.Core.ProcessInfo;
+using System;
 using System.Collections.Generic;
 using System.IO;
 
@@ -7,14 +9,17 @@ namespace MemoScope.Modules.Explorer
     public class FileData : AbstractDumpExplorerData
     {
         public override FileInfo FileInfo { get; }
+        ProcessInfo processInfo;
         public FileData(FileInfo fileInfo)
         {
             FileInfo = fileInfo;
             Name = fileInfo.Name;
             Icon = Properties.Resources.file_extension_bin;
+            var clrDumpInfo = ClrDumpInfo.Load(fileInfo.FullName);
+            processInfo = clrDumpInfo.ProcessInfo;
         }
 
-        public override long Size => FileInfo.Length/1000000;
+        public override long Size => FileInfo.Length / 1000000;
         public override bool CanExpand => false;
         public override List<AbstractDumpExplorerData> Children => null;
         public override string GetCachePath()
@@ -23,5 +28,64 @@ namespace MemoScope.Modules.Explorer
             return cachePath;
         }
 
+        public override long? HandleCount => processInfo?.HandleCount;
+        public override string CommandLine => processInfo?.CommandLine;
+        public override DateTime? DumpTime 
+        {
+            get
+            {
+                if(processInfo == null || processInfo.DumpTime == DateTime.MinValue)
+                {
+                    return null;
+                }
+                return processInfo.StartTime;
+            }
+        }
+
+        public override string MachineName => processInfo?.MachineName;
+        public override long? PagedMemory => processInfo?.PagedMemory / 1000000;
+        public override long? PeakPagedMemory => processInfo?.PeakPagedMemory / 1000000;
+        public override long? PeakVirtualMemory => processInfo?.PeakVirtualMemory / 1000000;
+        public override long? PeakWorkingSet => processInfo?.PeakWorkingSet / 1000000;
+        public override long? PrivateMemory => processInfo?.PrivateMemory / 1000000;
+        public override DateTime? StartTime
+        {
+            get
+            {
+                if(processInfo == null || processInfo.StartTime == DateTime.MinValue)
+                {
+                    return null;
+                }
+                return processInfo.StartTime;
+            }
+        }
+
+        public override TimeSpan? TotalProcessorTime
+        {
+            get
+            {
+                if(processInfo == null || processInfo.TotalProcessorTime == TimeSpan.Zero)
+                {
+                    return null;
+                }
+                return processInfo.TotalProcessorTime;
+            }
+        }
+
+        public override string UserName => processInfo?.UserName;
+        public override TimeSpan? UserProcessorTime
+        {
+            get
+            {
+                if (processInfo == null || processInfo.UserProcessorTime == TimeSpan.Zero )
+                {
+                    return null;
+                }
+                return processInfo.UserProcessorTime;
+            }
+        }
+        
+        public override long? VirtualMemory => processInfo?.VirtualMemory / 1000000;
+        public override long? WorkingSet => processInfo?.WorkingSet / 1000000;
     }
 }
