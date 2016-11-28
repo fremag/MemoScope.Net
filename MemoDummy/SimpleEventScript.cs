fce15a8958761af16b8800661e9a6dfbb70c903b
fremag <frederic.magri@gmail.com>
2016-11-28 19:14:17 +0100

    Memodummy: update EventScript to make a demo for MemoScope's Delegate analysis

--- MemoDummy/SimpleEventScript.cs
+++ MemoDummy/SimpleEventScript.cs
@@ -10,7 +10,7 @@ namespace MemoDummy
         public override string Description => "Create instances with one subscriber";
 
         [Category("Config")]
-        public long N { get; set; } = 10;
+        public long N { get; set; } = 300;
 
         private List<object> objects;
 
@@ -20,12 +20,18 @@ namespace MemoDummy
             for(int i=0; i < N; i++)
             {
                 var obj = new ClassWithEventHandlers();
-                obj.FirstEventHandler += MyCallBack;
+                obj.FirstEventHandler += MyFirstCallBack;
+                obj.SecondEventHandler += MySecondCallBack;
                 objects.Add(obj);
             }
         }
 
-        private void MyCallBack(object sender, EventArgs e)
+        private void MySecondCallBack(string s)
+        {
+            throw new NotImplementedException();
+        }
+
+        private int MyFirstCallBack(int x, int y)
         {
             throw new NotImplementedException();
         }
@@ -33,16 +39,19 @@ namespace MemoDummy
 
     public class ClassWithEventHandlers
     {
-        public event EventHandler FirstEventHandler;
-        public event EventHandler SecondEventHandler;
+        public delegate int MyIntDelegate(int x, int y);
+        public delegate void MyVoidDelegate(string s);
+
+        public event MyIntDelegate FirstEventHandler;
+        public event MyVoidDelegate SecondEventHandler;
 
         public void InvokeFirstEventHandler()
         {
-            if(FirstEventHandler != null) FirstEventHandler(this, EventArgs.Empty );
+            if(FirstEventHandler != null) FirstEventHandler(0,1 );
         }
         public void InvokeSecondEventHandler()
         {
-            if (SecondEventHandler != null) SecondEventHandler(this, EventArgs.Empty);
+            if (SecondEventHandler != null) SecondEventHandler(string.Empty);
         }
     }
 }
