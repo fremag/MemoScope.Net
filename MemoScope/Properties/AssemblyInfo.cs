b536c2814b99b1a28fa84e7345f390172d8976e1
fremag <fremag.gh@gmail.com>
2017-05-27 21:35:46 +0200

    Change date copyright (completely useless since the project is public domain...)

--- MemoScope/Properties/AssemblyInfo.cs
+++ MemoScope/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MemoScope")]
-[assembly: AssemblyCopyright("Copyright © FreMag 2016")]
+[assembly: AssemblyCopyright("Copyright © FreMag 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
